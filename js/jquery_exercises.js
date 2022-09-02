fc1516c65818f576323deeb7a28cd0609306d7a9
Justin Hollmer <justin.hollmer@gmail.com>
2022-09-02 16:53:25 -0500

    Completed jquery selectors exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,9 @@
-$(function() {
-   alert("The DOM has finished loading");
-});
+$("li").css("font-size", "20px");
+
+$("li, p, h1").css("background-color", "yellow");
+
+var headings = $("h1");
+
+for (let i = 0; i < headings.length; i++) {
+    alert(headings[i].innerHTML);
+}
